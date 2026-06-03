5810461f5e0e26f6aa23ff83bfa510dc8d75b040
Tomaz Xavier <tomaz.xavier@puravida.com.br>
2026-06-03 20:57:54 -0300

    chore: add env log

--- src/main/java/org/ufpr/oscarapi/OscarApiApplication.java
+++ src/main/java/org/ufpr/oscarapi/OscarApiApplication.java
@@ -1,12 +1,23 @@
 package org.ufpr.oscarapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OscarApiApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(OscarApiApplication.class);
+
     public static void main(String[] args) {
+        log.info("=== ENV VARS ===");
+        log.info("PGHOST={}", System.getenv("PGHOST"));
+        log.info("PGPORT={}", System.getenv("PGPORT"));
+        log.info("PGDATABASE={}", System.getenv("PGDATABASE"));
+        log.info("PGUSER={}", System.getenv("PGUSER"));
+        log.info("PGPASSWORD={}", System.getenv("PGPASSWORD") != null ? "***set***" : "NOT SET");
+        log.info("================");
         SpringApplication.run(OscarApiApplication.class, args);
     }
 
